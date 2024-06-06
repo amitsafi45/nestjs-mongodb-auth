b3099bb6e26289ff5aee54cafbcec1d0f72422e0
amitsafi45 <safiamit45@gmail.com>
2024-06-06 11:59:42 +0545

    add: configure Global error handling ,so that all exceptions error can log in to file later

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
+import { GlobalErrorHandlingFilter } from './utils/exceptionsFilter/globalErrorHandling.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const  httpAdapter  = app.get(HttpAdapterHost);
   app.use(helmet())
+  app.useGlobalFilters(new GlobalErrorHandlingFilter(httpAdapter));
   const PORT_NUMBER=app.get(ConfigService).get('PORT')
+
   await app.listen(PORT_NUMBER,()=>{
     console.log(`Server listening at http://localhost:${PORT_NUMBER}`)
   });
