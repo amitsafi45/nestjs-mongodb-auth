be59c05685bfd4a075bc78aae52dd8962ad49871
amitsafi45 <safiamit45@gmail.com>
2024-06-13 18:59:47 +0545

    fix: clean code

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -36,37 +36,3 @@ UserSchema.pre('save', async function save(next) {
   }
 });
 
-// export const UserSchema = new mongoose.Schema({
-//   email: {
-//     type: String,
-//     unique: true,
-//     lowercase: true,
-//   },
-//   password: {
-//     type: String,
-//   },
-//   gender: {
-//     type: String,
-//     enum: GENDER,
-//   },
-//   role: {
-//     type: [
-//       {
-//         type: String,
-//         enum: ROLE,
-//       },
-//     ],
-//     require: false,
-//     default: undefined,
-//   },
-// });
-
-// UserSchema.pre('save', async function save(next) {
-//   if (!this.isModified('password')) return next();
-//   try {
-//     this.password = await bcrypt.hash(this.password, SALT_WORK_FACTOR);
-//     return next();
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
